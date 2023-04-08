e362182a3e6d7923f6b4d7b321ee8998e832bb6c
JYOTIPM1999 <jyotipm1999@gmail.com>
2023-04-08 12:46:56 +0530

    Date picked added but have some issue

--- client/src/pages/Transaction/Transaction.jsx
+++ client/src/pages/Transaction/Transaction.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
   Table,
   Thead,
@@ -8,16 +8,35 @@ import {
   Td,
   TableContainer,
   Button,
+  useDisclosure,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  Text,
+  ModalFooter,
+  Modal,
+  Stack,
+  Input,
+  InputGroup,
+  FormControl,
+  FormLabel,
+  Flex,
+  Select,
 } from "@chakra-ui/react";
-
 import data from "../../data/db.json";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Transaction = () => {
-  // const [database, setDatabase] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(null);
 
-  // useEffect(() => {
-  //   setDatabase(data.data);
-  // }, []);
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+  console.log(selectedDate);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const groupedData = data.data.reduce((acc, item) => {
     if (!acc[item.date]) {
@@ -34,9 +53,163 @@ const Transaction = () => {
         right={"100px"}
         top={"20px"}
         colorScheme="orange"
+        onClick={onOpen}
       >
-        ADD
+        ADD TRANSACTION
       </Button>
+      <Modal
+        blockScrollOnMount={false}
+        isOpen={isOpen}
+        onClose={onClose}
+        size={"xl"}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add Transaction</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text fontWeight="bold" mb="1rem">
+              You can scroll the content behind the modal
+            </Text>
+
+            <Flex justify={"center"}>
+              <Stack>
+                <FormControl isRequired>
+                  <FormLabel>Select Type</FormLabel>
+                  <Select placeholder="Select option">
+                    <option value="Income">Income</option>
+                    <option value="Expense">Expense</option>
+                  </Select>
+                </FormControl>
+
+                <FormControl isRequired>
+                  <FormLabel>Select Date</FormLabel>
+                  <div>
+                    <DatePicker
+                      selected={selectedDate}
+                      onChange={handleDateChange}
+                      dateFormat="yyyy-MM-dd"
+                      placeholderText="Ex:-2023-04-06"
+                    />
+                    {selectedDate && (
+                      <div>
+                        <select
+                          value={selectedDate.getDate()}
+                          onChange={(e) =>
+                            setSelectedDate(
+                              new Date(
+                                selectedDate.getFullYear(),
+                                selectedDate.getMonth(),
+                                e.target.value
+                              )
+                            )
+                          }
+                        >
+                          {[...Array(31).keys()].map((day) => (
+                            <option key={day} value={day + 1}>
+                              {day + 1}
+                            </option>
+                          ))}
+                        </select>
+                        <select
+                          value={selectedDate.getMonth()}
+                          onChange={(e) =>
+                            setSelectedDate(
+                              new Date(
+                                selectedDate.getFullYear(),
+                                e.target.value,
+                                selectedDate.getDate()
+                              )
+                            )
+                          }
+                        >
+                          {[
+                            "January",
+                            "February",
+                            "March",
+                            "April",
+                            "May",
+                            "June",
+                            "July",
+                            "August",
+                            "September",
+                            "October",
+                            "November",
+                            "December",
+                          ].map((month, index) => (
+                            <option key={index} value={index}>
+                              {month}
+                            </option>
+                          ))}
+                        </select>
+                        <select
+                          value={selectedDate.getFullYear()}
+                          onChange={(e) =>
+                            setSelectedDate(
+                              new Date(
+                                e.target.value,
+                                selectedDate.getMonth(),
+                                selectedDate.getDate()
+                              )
+                            )
+                          }
+                        >
+                          {[...Array(10).keys()].map((year) => (
+                            <option
+                              key={year}
+                              value={selectedDate.getFullYear() + year}
+                            >
+                              {selectedDate.getFullYear() + year}
+                            </option>
+                          ))}
+                        </select>
+                      </div>
+                    )}
+                  </div>
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Mode of Transaction</FormLabel>
+                  <Select placeholder="Select option">
+                    <option value="Cash">Cash</option>
+                    <option value="Account">Account</option>
+                    <option value="Card">Card</option>
+                  </Select>
+                </FormControl>
+                <FormControl isRequired>
+                  <FormLabel>Category</FormLabel>
+                  <Select placeholder="Select Option">
+                    <option value="Food">Food</option>
+                    <option value="Transportation">Transportation</option>
+                    <option value="Recharge">Recharge</option>
+                  </Select>
+                </FormControl>
+                <FormControl isRequired>
+                  <FormLabel>Amount</FormLabel>
+                  <InputGroup>
+                    <Input type="number" />
+                  </InputGroup>
+                </FormControl>
+                <FormControl isRequired>
+                  <FormLabel>Description</FormLabel>
+                  <InputGroup>
+                    <Input type="text" />
+                  </InputGroup>
+                </FormControl>
+              </Stack>
+            </Flex>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="red" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="solid" colorScheme="green" onClick={onClose}>
+              Save
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
       <TableContainer>
         {Object.keys(groupedData).map((date) => {
           return (
